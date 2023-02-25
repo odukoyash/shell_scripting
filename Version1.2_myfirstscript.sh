b04356b41a24794cc22c89e91db0d83c8dd58267
odukoyash <sodukoya122@outlook.com>
2023-02-25 13:14:05 -0600

    mybackup

--- Version1.2_myfirstscript.sh
+++ Version1.2_myfirstscript.sh
@@ -5,27 +5,33 @@
 #Acceptance criterial:Given I am a Cloud Engineer,When I run my backup script with command line arguments, Then the backup file is created in the runner's backup location path
 
 #File to be backup
-#creating a directory to backyup
-#variable substitution
+#creating a directory to backup
+
+#COMMAND LINE ARGUEMENT
+#Passing argument into a script or file
+
+
+backup_location=$1
+
 export PRACTICEDIR="/c/apps/terraform/tf/stackit/practicedir_jul22_shina"
-export DIR="backup"
+#export DIR=$1
 export FILE="myfirstfile.txt"
 
 
 #creating my first directory
-echo " I will be creating my first directory"
-mkdir ${PRACTICEDIR}/${DIR}
+echo " I will be creating my first backup directory"
+mkdir ${PRACTICEDIR}/${backup_location}
 
 #copying my first file into my first directory
-echo "I will be copying my first file into my directory"
-cp ${PRACTICEDIR}/${FILE} ${PRACTICEDIR}/${DIR}
+echo "I will be copying my first file into my backup directory"
+cp ${PRACTICEDIR}/${FILE} ${PRACTICEDIR}/${backup_location}
 
 #Listing the content of my firstdir
-echo " I will be listing the content of my first directory "
-ls -ltr ${PRACTICEDIR}/${DIR}
+echo " I will be listing the content of my first backup directory "
+ls -ltr ${PRACTICEDIR}/${backup_location}
 
-#counting the content of my firstdirectory
-ls -ltr ${PRACTICEDIR}/${DIR}|wc -l
+#counting the content of my first backup directory
+ls -ltr ${PRACTICEDIR}/${backup_location}|wc -l
 
 
 
