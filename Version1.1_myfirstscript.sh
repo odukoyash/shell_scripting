2dcca509052f415bbe41e3f8c692c0cca9c80e83
odukoyash <sodukoya122@outlook.com>
2023-02-25 11:56:42 -0600

    Variable subsistution

--- Version1.1_myfirstscript.sh
+++ Version1.1_myfirstscript.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+#variable substitution
+export PRACTICEDIR="/c/apps/terraform/tf/stackit/practicedir_jul22_shina"
+export DIR="myfirstdir"
+export FILE="myfirstfile.txt"
+
+
 #creating my first directory
 echo " I will be creating my first directory"
-mkdir  /c/apps/terraform/tf/stackit/practicedir_jul22_shina/myfirstdir
-
-#creating and writing a file
-echo " I love working at stackit solution, its the best place to be" >> /c/apps/terraform/tf/stackit/practicedir_jul22_shina/myfirstfile.txt
+mkdir ${PRACTICEDIR}/${DIR}
 
 #copying my first file into my first directory
 echo "I will be copying my first file into my directory"
-cp /c/apps/terraform/tf/stackit/practicedir_jul22_shina/myfirstfile.txt /c/apps/terraform/tf/stackit/practicedir_jul22_shina/myfirstdir
+cp ${PRACTICEDIR}/${FILE} ${PRACTICEDIR}/${DIR}
 
 #Listing the content of my firstdir
 echo " I will be listing the content of my first directory "
-ls -ltr /c/apps/terraform/tf/stackit/practicedir_jul22_shina/myfirstdir
+ls -ltr ${PRACTICEDIR}/${DIR}
+
+
 
 echo " end of my script"
