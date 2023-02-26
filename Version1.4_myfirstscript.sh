067b8b0645fd7856e2a25e91333497e7665cd939
odukoyash <sodukoya122@outlook.com>
2023-02-26 12:58:19 -0600

    backup a directory with a timestamp

--- Version1.4_myfirstscript.sh
+++ Version1.4_myfirstscript.sh
@@ -4,14 +4,16 @@
 #UserStory :As a Cloud Engineer,I want to pass command line arguments to my backup script,so that my backup script can execute dynamically
 #Acceptance criterial:Given I am a Cloud Engineer,When I run my backup script with command line arguments, Then the backup file is created in the runner's backup location path
 
-#File to be backup
+# to be backup a directory
 #creating a directory to backup
 
 #COMMAND LINE ARGUEMENT
 #Passing argument into a script or file
 
-FILE=$1
+DIR=$1
 BACKUP_LOCATION=$2
+RUNNER=$3
+
 
 PRACTICEDIR="/c/apps/terraform/tf/stackit/practicedir_jul22_shina"
 FINAL_BACKUP_LOCATION=${PRACTICEDIR}/${BACKUP_LOCATION}
@@ -19,11 +21,11 @@ FINAL_BACKUP_LOCATION=${PRACTICEDIR}/${BACKUP_LOCATION}
 
 #creating my first backup directory location
 echo " I will be creating my first backup directory"
-mkdir ${FINAL_BACKUP_LOCATION}
+mkdir -p ${DIR}
 
 #copying a file(s) into my backup location
 echo "I will be copying file(s) into my backup location"
-cp ${FILE} ${FINAL_BACKUP_LOCATION}
+cp -R ${DIR} ${FINAL_BACKUP_LOCATION}
 
 #Listing the content of my firstdir
 echo " I will be listing the content of  backup location "
